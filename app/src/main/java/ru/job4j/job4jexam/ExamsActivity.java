c039d08479604cf0f2ca22ce4726ed6dc9881fdb
Ksenya Kaysheva <murrcha@me.com>
2018-08-16 22:08:20 +0400

    2. Виджет - список. [#61533]

--- app/src/main/java/ru/job4j/job4jexam/ExamsActivity.java
+++ app/src/main/java/ru/job4j/job4jexam/ExamsActivity.java
@@ -1,108 +1,58 @@
 package ru.job4j.job4jexam;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import ru.job4j.job4jexam.adapter.ExamAdapter;
 import ru.job4j.job4jexam.pojo.Exam;
 
 public class ExamsActivity extends AppCompatActivity {
 
-    private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
-
+    /**
+     * recycler
+     */
     private RecyclerView recycler;
+
+    /**
+     * adapter
+     */
     private ExamAdapter adapter;
 
-    private void updateUI() {
+    /**
+     * Method getExams generate exams list
+     * @return
+     */
+    private List<Exam> getExams() {
         List<Exam> exams = new ArrayList<>();
         for (int index = 0; index <= 100; index++) {
             exams.add(new Exam(index, String.format("Exam %s", index), System.currentTimeMillis(), index));
         }
-        adapter = new ExamAdapter(exams);
-        this.recycler.setAdapter(adapter);
+        return exams;
     }
 
+    /**
+     * Method initRecycler init recycler
+     */
+    private void initRecyclerView() {
+        recycler = findViewById(R.id.exams_recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ExamAdapter();
+        adapter.setItems(getExams());
+        recycler.setAdapter(adapter);
+    }
+
+    /**
+     * ${@inheritDoc}
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exams);
-        recycler = findViewById(R.id.exams_recycler);
-        recycler.setLayoutManager(new LinearLayoutManager(this));
-        updateUI();
-    }
-
-    private class ExamAdapter extends RecyclerView.Adapter<ExamListHolder> {
-
-        private final List<Exam> exams;
-
-        public ExamAdapter(List<Exam> exams) {
-            this.exams = exams;
-        }
-
-        @Override
-        public int getItemCount() {
-            return this.exams.size();
-        }
-
-        @Override
-        public ExamListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            View view = inflater.inflate(R.layout.info_layout_item, parent, false);
-            return new ExamListHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(ExamListHolder holder, int position) {
-            holder.bind(this.exams.get(position));
-        }
-    }
-
-    private class ExamListHolder extends RecyclerView.ViewHolder {
-
-        private TextView idText;
-        private TextView infoText;
-        private TextView resultText;
-        private TextView dateText;
-        private Exam exam;
-
-        public ExamListHolder(View itemView) {
-            super(itemView);
-            idText = itemView.findViewById(R.id.id_text);
-            infoText = itemView.findViewById(R.id.info_text);
-            resultText = itemView.findViewById(R.id.result_text);
-            dateText = itemView.findViewById(R.id.date_text);
-        }
-
-        public void bind(Exam exam) {
-            this.exam = exam;
-            idText.setText(String.valueOf(this.exam.getId()));
-            infoText.setText(this.exam.getName());
-            resultText.setText(String.valueOf(this.exam.getResult()));
-            SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
-            Date date = new Date(this.exam.getTime());
-            dateText.setText(sdf.format(date));
-            infoText.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(ExamsActivity.this, ExamActivity.class));
-                    Toast.makeText(
-                            getApplicationContext(), "You select " + ExamListHolder.this.exam,
-                            Toast.LENGTH_SHORT
-                    ).show();
-                }
-            });
-        }
+        initRecyclerView();
     }
 }
---
+++ app/src/main/java/ru/job4j/job4jexam/adapter/ExamAdapter.java
@@ -0,0 +1,132 @@
+package ru.job4j.job4jexam.adapter;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import ru.job4j.job4jexam.ExamActivity;
+import ru.job4j.job4jexam.R;
+import ru.job4j.job4jexam.pojo.Exam;
+
+/**
+ * ExamAdapter
+ *
+ * @author Ksenya Kaysheva (murrcha@me.com)
+ * @since 0.1
+ */
+public class ExamAdapter extends RecyclerView.Adapter<ExamAdapter.ExamViewHolder> {
+
+    /**
+     * List exams
+     */
+    private final List<Exam> exams = new ArrayList<>();
+
+    /**
+     * ${@inheritDoc}
+     */
+    @Override
+    public ExamViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.info_layout_item, parent, false);
+        return new ExamViewHolder(view);
+    }
+
+    /**
+     * ${@inheritDoc}
+     */
+    @Override
+    public void onBindViewHolder(ExamViewHolder holder, int position) {
+        holder.bind(exams.get(position));
+    }
+
+    /**
+     * ${@inheritDoc}
+     */
+    @Override
+    public int getItemCount() {
+        return exams.size();
+    }
+
+    /**
+     * Method setItems add new items to exams list
+     * @param exams
+     */
+    public void setItems(Collection<Exam> exams) {
+        this.exams.addAll(exams);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Method clearItems clear exams list
+     */
+    public void clearItems() {
+        this.exams.clear();
+        notifyDataSetChanged();
+    }
+
+    /**
+     * ExamViewHolder
+     */
+    class ExamViewHolder extends RecyclerView.ViewHolder {
+
+        /**
+         * Date format
+         */
+        private static final String DATE_FORMAT = "dd.MM.yyyy HH:mm";
+
+        /**
+         * UI elements
+         */
+        private TextView idText;
+        private TextView infoText;
+        private TextView resultText;
+        private TextView dateText;
+        private Exam exam;
+
+        /**
+         * Init view holder
+         * @param itemView view
+         */
+        public ExamViewHolder(View itemView) {
+            super(itemView);
+            idText = itemView.findViewById(R.id.id_text);
+            infoText = itemView.findViewById(R.id.info_text);
+            resultText = itemView.findViewById(R.id.result_text);
+            dateText = itemView.findViewById(R.id.date_text);
+        }
+
+        /**
+         * Method bind binding view and data from exam
+         * @param exam
+         */
+        public void bind(Exam exam) {
+            this.exam = exam;
+            idText.setText(String.valueOf(this.exam.getId()));
+            infoText.setText(this.exam.getName());
+            resultText.setText(String.valueOf(this.exam.getResult()));
+            SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+            Date date = new Date(this.exam.getTime());
+            dateText.setText(sdf.format(date));
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemView.getContext().startActivity(new Intent(itemView.getContext(), ExamActivity.class));
+                    Toast.makeText(
+                            itemView.getContext(), "You select " + ExamViewHolder.this.exam,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            });
+        }
+    }
+}
